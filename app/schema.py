7e287912932b84459082ad5d13348f2df755057c
bslamek <bslamek@idc.com>
2023-06-10 17:31:18 -0300

    add safe delete

--- app/schema.py
+++ app/schema.py
@@ -19,27 +19,42 @@ class CreateType(graphene.Mutation):
 
     @staticmethod
     def mutate(self, info, name):
+        exists = Type.objects.filter(name=name, is_active=True).exists()
+        if exists:
+            return Exception('Already exists!')
+        
         obj = Type(name=name)
         obj.save()
 
-        return CreateType(
-            name=obj.name,
-        )
+        return CreateType(name=obj.name)
+    
+
+class DeleteType(graphene.Mutation):
+    class Arguments:
+        type_id = graphene.ID(required=True)
+
+    success = graphene.Boolean()
+
+    @staticmethod
+    def mutate(root, info, type_id):
+        obj = Type.objects.get(id=type_id)
+        obj.is_active = False
+        obj.save()
+        return DeleteType(success=True)
 
 
 class Mutation(graphene.ObjectType):
     create_type = CreateType.Field()
+    delete_type = DeleteType.Field()
 
 
 class Query(graphene.ObjectType):
-    """ hello = graphene.String(default_value="Hello World! GraphQL with Django!") """
     types = graphene.List(TypeSerializer)
-    # type_by_id = graphene.Field(TypeSerializer, name=graphene.String(required=True))
     type_by_id = graphene.Field(TypeSerializer, type_id=graphene.ID())
 
     @staticmethod
     def resolve_types(root, info):
-        return Type.objects.filter()
+        return Type.objects.filter(is_active=True)
 
     @staticmethod
     def resolve_type_by_id(root, info, type_id):
