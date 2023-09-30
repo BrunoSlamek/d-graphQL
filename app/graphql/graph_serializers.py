f1f8266ca06335bcc8159f8d2296dcb7a8f94bf0
Bruno Slamek <brunoslamek2@gmail.com>
2023-09-29 21:58:00 -0300

    add models users

--- app/graphql/graph_serializers.py
+++ app/graphql/graph_serializers.py
@@ -1,8 +1,16 @@
 from graphene_django import DjangoObjectType
 from ..models import Type
 
+from django.contrib.auth.models import User
+
 
 class TypeSerializer(DjangoObjectType):
     class Meta:
         model = Type
         fields = ('id', 'name')
+
+
+class UserSerializer(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = "__all__"
--- app/schema.py
+++ app/schema.py
@@ -2,22 +2,30 @@ import graphene
 
 from .models import Type
 
-from .graphql.graph_serializers import TypeSerializer
+from .graphql.graph_serializers import TypeSerializer, UserSerializer
 from .graphql.graph_api import Mutation
+
+
+from django.contrib.auth.models import User
 
 
 class Query(graphene.ObjectType):
     types = graphene.List(TypeSerializer)
     type_by_id = graphene.Field(TypeSerializer, type_id=graphene.ID())
+    users = graphene.List(UserSerializer)
 
     @staticmethod
     def resolve_types(root, info):
+        print(info.context.user)
         return Type.return_all_active()
 
     @staticmethod
     def resolve_type_by_id(root, info, type_id):
         return Type.get_by_id(type_id=type_id)
 
+    @staticmethod
+    def resolve_users(root, info):
+        return User.objects.all()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
@@ -33,5 +41,17 @@ schema = graphene.Schema(query=Query, mutation=Mutation)
 """ mutation {
   deleteType(typeId: 1) {
     success
+  }
+} """
+
+""" query {
+  types {
+    id
+    name
+  }
+  users {
+    id
+    username
+    isSuperuser
   }
 } """
