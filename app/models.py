690e388b1723dd37cab09caebacd8d38d6f8a6ea
Bruno Slamek <brunoslamek2@gmail.com>
2023-09-30 11:39:21 -0300

    fix on_delete

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class Type(models.Model):
     name = models.CharField(max_length=256)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         db_table: str = 'tb_type'
